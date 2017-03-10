84501a8163a36fbf1c96c7ebe00d9ff528eadada
hamache9821 <hamache.9821@gmail.com>
2017-03-09 20:39:42 -0900

    とりあえず動くやつ

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # ImacocoClient-nodejs
 ----
 ### なにこれ
-nodejsで今ココにpostしたい  
-* 秒速1.7mで移動するサーバの実験用途  
-
-#### 進捗ダメです  
+nodejsで今ココにpostするやつ  
+* 秒速1.7mで移動するRaspi2の実験用途  
 
 
 ### 動作環境  
-* Node.js の動くraspi2 + 3GPiを想定  
+* Node.js の動くraspi2 + 3GPiのGPS  
+* NMEA-0183形式でGGAセンテンスを1Hzでttyに吐き続けるGPSモジュールだと何でもいけるはず  
+
+### 動かす  
+
+* config/default.json をいい感じに編集すること
+
+
+```sh
+$ git clone https://github.com/hamache9821/ImacocoClient-nodejs.git
+$ cd ImacocoClient-nodejs
+$ npm install
+$ npm start
+```
+
 
--- app.js
+++ app.js
@@ -1,66 +1,80 @@
 "use strict";
 
 const config = require('config')
-    , gpsd = require('node-gpsd');
+    , sp  = require('serialport')
+    , GPS = require('gps')
+    , http = require('http')
+    , querystring = require('querystring')
+    , moment = require('moment-timezone');
 
-//必須設定チェック
-if (!config.gpsd_device) {throw 'config `gpsd_device` is not set';}
-if (!config.gpsd_port)   {throw 'config `gpsd_port` is not set';}
-if (!config.gpsd_pid)    {throw 'config `gpsd_pid` is not set';}
-if (!config.post_url)    {throw 'config `post_url` is not set';}
-if (!config.post_userid) {throw 'config `post_userid` is not set';}
-if (!config.post_passwd) {throw 'config `post_passwd` is not set';}
-if (!config.post_timer)  {throw 'config `post_timer` is not set';}
+var cnt = 0;
 
-const daemon = new gpsd.Daemon({
-    program: 'gpsd',
-    device   : config.gpsd_device,
-    port     : config.gpsd_port,
-    pid      : config.gpsd_pid,
-    readOnly : false,
-    logger   : {
-        info : function() {},
-        warn : console.warn,
-        error: console.error
+if (!config.gps_device)     {throw 'config `gps_device` is not set';}
+if (!config.gps_baudrate)   {throw 'config `gps_baudrate` is not set';}
+if (!config.post_hostname)  {throw 'config `post_hostname` is not set';}
+if (!config.post_path)      {throw 'config `post_path` is not set';}
+if (!config.post_port)      {throw 'config `post_port` is not set';}
+if (!config.post_userid)    {throw 'config `post_userid` is not set';}
+if (!config.post_passwd)    {throw 'config `post_passwd` is not set';}
+if (!config.post_timer)     {throw 'config `post_timer` is not set';}
+
+const port =
+    new sp.SerialPort(config.gps_device, {
+      baudrate : config.gps_baudrate,
+      parser   : sp.parsers.readline('\r\n')
+    })
+   , gps = new GPS;
+
+
+port.on('data', function(data) {
+  gps.update(data);
+});
+
+
+gps.on('data', function(data) {
+    switch (data.type){
+        case 'GGA':
+            cnt ++;
+
+            if (cnt >= config.post_timer){
+                cnt = 0;
+                var qs = querystring.stringify({
+                    time : moment(data.time).tz("Asia/Tokyo").format(),
+                    lat  : data.lat,
+                    lon  : data.lon,
+                    gpsq : data.quality,
+                    gpsn : data.satelites,
+                    gpsh : data.alt,
+                    gpsd : data.hdop,
+                    gpsv : data.geoidal,
+                    save : 0,
+                    t    : 0
+                });
+
+                var url = {
+                        host : config.post_hostname,
+                        path : config.post_path,
+                        port : config.post_port,
+                        method : 'POST',
+                        headers : {
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            'Content-Length': qs.length
+                        },
+                        auth : config.post_userid + ':' + config.post_passwd
+                    };
+
+                    var req = http.request(url, function (res) {
+                        res.on('data', function (chunk) {
+                            console.log(moment(data.time).tz("Asia/Tokyo").format() + ' res:' + chunk.toString());
+
+                        }).on('error', function (e) {
+                            console.log('ERROR:' + e.stack);
+                        });
+                    });
+
+                    req.write(qs);
+                    req.end();
+            }
+        default:
     }
 });
-
-const listener = new gpsd.Listener({
-    port     : config.gpsd_port,
-    hostname : 'localhost',
-    logger   : {
-        info  : function() {},
-        warn  : console.warn,
-        error : console.error
-    },
-    parse: true
-});
-
-/*
-daemon.logger = new (winston.Logger) ({ exitOnError: false });
-listener.logger = new (winston.Logger) ({ exitOnError: false });;
-*/
-
-daemon.start(function() {
-    console.log('Started');
-});
-
-/*
-daemon.stop(function() {
-    console.log('Stopped');
-});
-*/
-
-listener.connect(function() {
-    console.log('Connected');
-});
-
-/*
-listener.disconnect(function() {
-    console.log('Disconnected');
-});
-*/
-
-
-
-
